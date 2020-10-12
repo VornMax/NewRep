5e388d385d086b9b8d7ea070de35d44afcbf0fa5
Voronko Maks <mars-sk8board@mail.ru>
2020-10-12 10:16:15 +0300

    add class user

---
+++ user.rb
@@ -0,0 +1,28 @@
+class User
+
+  def initialize(array)
+    @user_array = array
+  end
+
+
+	def map(&block)
+	  result = []
+	  each do |element|
+	    #result << block.call(element)
+	    result << x if yield(element)
+	  end
+	  result
+	end
+
+  def each(&block)
+    @user_array.each(&block)
+  end
+
+end
+
+
+user_array_instance = User.new(%w[maks1 vanya1 sasha3 foo2 boo1])
+user_array_instance.map {|n| puts n}
+
+
+
