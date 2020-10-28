da32c140dceeb86f694ede6a5e78a6cc6618e68f
Voronko Maks <mars-sk8board@mail.ru>
2020-10-28 10:19:48 +0300

    Correct queen_attack.rb with rubocop

--- queen_attack/queen_attack.rb
+++ queen_attack/queen_attack.rb
@@ -1,32 +1,33 @@
+# frozen_string_literal: true
+
+# Class Queens for chess.
 class Queens
-	def initialize(white = nil, black = nil)
-		begin
-	    @white = white
-	    @black = black
-	  raise ArgumentError, "You don't enter a position." if correct_value?	 
-	  rescue => mes
-	  	p mes
-	  end
+  def initialize(white = nil, black = nil)
+    @white = white
+    @black = black
+    raise ArgumentError, "You don't enter a position." if correct_value?
+  rescue StandardError => e
+    p e
   end
 
   def create_field
-  	n = 8
-  	array = Array.new(n) { |i| Array.new(n) { |i| "_" }}
-  	for i in 0..n
-	  	for j in 0..n
-	  			array[@white[0]][@white[1]] = "W"
-	  			array[@black[0]][@black[1]] = "B"
-	  	end
-	  end
-  	print_array(array)
-  	puts attack?
+    n = 8
+    array = Array.new(n) { |_i| Array.new(n) { |_i| '_' } }
+    (0..n).each do |_i|
+      (0..n).each do |_j|
+        array[@white[0]][@white[1]] = 'W'
+        array[@black[0]][@black[1]] = 'B'
+      end
+    end
+    print_array(array)
+    puts attack?
   end
 
   private
 
   def correct_value?
-    @black == nil || @black.all? { |i| i >= 0 && i <= 7 } 
-    @white == nil || @white.all? { |i| i >= 0 && i <= 7 }
+    @black.nil? || @black.all? { |i| i >= 0 && i <= 7 }
+    @white.nil? || @white.all? { |i| i >= 0 && i <= 7 }
   end
 
   def attack?
@@ -38,28 +39,26 @@ class Queens
   end
 
   def same_col?
-  	@white[1] == @black[1]
+    @white[1] == @black[1]
   end
 
   def same_diag?
-  	(@white[0] - @black[0]).abs == (@white[1] - @black[1]).abs
+    (@white[0] - @black[0]).abs == (@white[1] - @black[1]).abs
   end
 
   def print_array(array)
-  	array.each do |line|
-			line.each do |elem|
-				print elem 
-			end
-			print "\n"
-	  end
-	  puts ""
+    array.each do |line|
+      line.each do |elem|
+        print elem
+      end
+      print "\n"
+    end
+    puts ''
   end
 end
 
-
-#qa = Queens.new(white = [5, 6], black = [7, 4])	
-qa = Queens.new(white = [4, 6], black = [4, 4])	
-#qa = Queens.new(black = [1, 1])
-#qa = Queens.new( white = [2, 4])
+# qa = Queens.new(white = [5, 6], black = [7, 4])
+qa = Queens.new([4, 6], [4, 4])
+# qa = Queens.new(black = [1, 1])
+# qa = Queens.new( white = [2, 4])
 qa.create_field
-
